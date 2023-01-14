54b8d510979358b4551f988fae1563868cf3ee12
[jericovtn] <[jericojamesfviteno@iskolarngbayn.pup.edu.ph]>
2023-01-14 18:31:16 +0800

    Added: method addChild and check data to add

--- binary tree index.py
+++ binary tree index.py
@@ -8,4 +8,22 @@ class BinarySearchTreeNode:
         self.left = None
         self.right = None
 
+    def addChild(self, data):
+        if data == self.data:
+            return 
+            # Status: Node Exist
+        
+        if data < self.data:
+            # Add: Data in left subtree
+            if self.left:
+                self.left.addChild(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        
+        else:
+            # Add: Data in right subtree
+            if self.right:
+                self.right.addChild(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
 
